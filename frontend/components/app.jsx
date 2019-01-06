e5072f6e69a2203a6e54f5c0cbd5862f33b981db
Paul van Duyn <paul.vanduyn@gmail.com>
2019-01-06 12:34:45 -0500

    fix background image

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import { AuthRoute,
 import Login from './auth/login_container';
 import Signup from './auth/signup_container';
 import Nav from './nav/nav';
+import Background from './general/background';
 import Main from './main/main_container';
 import Account from './account/account_container';
 import AddMatch from './match/add_match_container';
@@ -12,6 +13,7 @@ import AddMatch from './match/add_match_container';
 export default () => {
   return (
     <>
+      <Background />
       <Nav />
       <Switch>
         <AuthRoute exact path='/login' component={ Login } />
